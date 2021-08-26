021e308b74f54fc5c32d208ebf42f2d7153998ed
Alexander Okun <74727162+MrOkun@users.noreply.github.com>
2021-08-27 01:20:28 +0300

    mobile version add.

---
+++ js/platform.js
@@ -0,0 +1,26 @@
+var userDeviceArray = [
+    {device: 'Android', platform: /Android/},
+    {device: 'iPhone', platform: /iPhone/},
+    {device: 'iPad', platform: /iPad/},
+    {device: 'Symbian', platform: /Symbian/},
+    {device: 'Windows Phone', platform: /Windows Phone/},
+    {device: 'Tablet OS', platform: /Tablet OS/},
+    {device: 'Linux', platform: /Linux/},
+    {device: 'Windows', platform: /Windows NT/},
+    {device: 'Macintosh', platform: /Macintosh/}
+];
+
+var platform = navigator.userAgent;
+
+function getPlatform() {
+    for (var i in userDeviceArray) {
+        if (userDeviceArray[i].platform.test(platform)) {
+            return userDeviceArray[i].device;
+        }
+    }
+    return 'Неизвестная платформа!' + platform;
+}
+
+if(getPlatform() != "Windows" && getPlatform() != "Linux" && getPlatform() != "Macintosh" && getPlatform() != "Tablet OS"){
+    window.open('/index-mobile.html');
+}
